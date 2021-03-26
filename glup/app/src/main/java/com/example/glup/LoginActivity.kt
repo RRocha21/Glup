842432b582f8509af0ad14ba211023fcd722c4a3
RRocha21 <renatorocha21@ua.pt>
2021-03-26 16:21:34 +0000

    1.1

--- glup/app/src/main/java/com/example/glup/LoginActivity.kt
+++ glup/app/src/main/java/com/example/glup/LoginActivity.kt
@@ -1,11 +1,37 @@
 package com.example.glup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        Button callSignUp;
+
+        findViewById<Button>(R.id.button_sign_in).setOnClickListener {
+            openMainActivity()
+        }
+
+        findViewById<Button>(R.id.button_new_user).setOnClickListener {
+            openRegistrationActivity()
+        }
+
+        callSignUp = findViewById<Button>(R.id.button_new_user)
+
+    }
+
+    private fun openMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun openRegistrationActivity() {
+        val intent = Intent(this, RegistrationActivity::class.java)
+        startActivity(intent)
     }
 }
