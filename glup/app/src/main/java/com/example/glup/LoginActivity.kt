747db323bf329117f87d649449b6a09f39f5ea5d
RRocha21 <renatorocha21@ua.pt>
2021-03-26 16:24:19 +0000

    1.1.1

--- glup/app/src/main/java/com/example/glup/LoginActivity.kt
+++ glup/app/src/main/java/com/example/glup/LoginActivity.kt
@@ -11,8 +11,6 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        Button callSignUp;
-
         findViewById<Button>(R.id.button_sign_in).setOnClickListener {
             openMainActivity()
         }
@@ -20,8 +18,6 @@ class LoginActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button_new_user).setOnClickListener {
             openRegistrationActivity()
         }
-
-        callSignUp = findViewById<Button>(R.id.button_new_user)
 
     }
 
