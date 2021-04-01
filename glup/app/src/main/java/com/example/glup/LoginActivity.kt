26b116b2a2bed59ff5886dd11e103cabed5bd279
RRocha21 <renatorocha21@ua.pt>
2021-04-01 17:09:51 +0100

    Renato Rocha
    acti

--- glup/app/src/main/java/com/example/glup/LoginActivity.kt
+++ glup/app/src/main/java/com/example/glup/LoginActivity.kt
@@ -38,7 +38,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun openforgotpass() {
-        val intent = Intent(this, ForgotActivity::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 
--- glup/app/src/main/java/com/example/glup/SettingsActivity.kt
+++ glup/app/src/main/java/com/example/glup/SettingsActivity.kt
@@ -1,11 +1,27 @@
 package com.example.glup
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity :AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+
+        val switch: Switch = findViewById(R.id.switch_dark)
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                openforgotpass()
+            } else {
+                // The toggle is disabled
+            }
+        }
+    }
+    private fun openforgotpass() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
