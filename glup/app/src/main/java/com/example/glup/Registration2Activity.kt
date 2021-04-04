0bd4ce2dbebe97d6c0ed23245c665530924b658d
RRocha21 <renatorocha21@ua.pt>
2021-04-04 19:34:31 +0100

    Renato Rocha 1.1.9

---
+++ glup/app/src/main/java/com/example/glup/Registration2Activity.kt
@@ -0,0 +1,157 @@
+package com.example.glup
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+
+
+
+
+class Registration2Activity : AppCompatActivity() {
+
+    private lateinit var regname: EditText
+    private lateinit var reguser: EditText
+    private lateinit var regnif: EditText
+    private lateinit var regphone:EditText
+    private lateinit var user: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration2)
+
+
+
+        // get what was written
+        regname  = findViewById<EditText>(R.id.registration_full_name)
+        reguser  = findViewById<EditText>(R.id.registration_username)
+        regnif   = findViewById<EditText>(R.id.registration_nif)
+        regphone = findViewById<EditText>(R.id.registration_phone)
+
+        // When sign up button is pressed
+        findViewById<Button>(R.id.button_sign_up).setOnClickListener {
+            RegisterUserActivity()
+
+        }
+        // When back button is pressed
+        findViewById<Button>(R.id.button_old_user2).setOnClickListener {
+            openLoginActivity()
+        }
+
+    }
+
+
+    private fun RegisterUserActivity() {
+
+        if(!validateName() or !validateUser() or  !validatePhone() or !validateNif()) {
+            return
+        }
+
+        // Prepare the Authentication
+        user= FirebaseAuth.getInstance()
+
+        // Prepare the database
+        val database = FirebaseDatabase.getInstance()
+        val newuser = database.getReference("users")
+
+
+        // more variables and put the text in String format
+        val name = regname.text.toString()
+        val userid = reguser.text.toString()
+        val nif = regnif.text.toString()
+        val phone = regphone.text.toString()
+        val balance = 0
+        val image = R.drawable.user_icon_app
+
+
+        val new_user = UserHelper(userid, name, nif, phone, balance, image)
+
+        val cuser = user.currentUser
+
+        // write in the database
+        newuser.child(cuser.uid).setValue(new_user)
+
+        // going to next activity
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+    }
+
+    private fun validateName(): Boolean {
+        val val_name = regname.text.toString()
+
+        if (val_name.isEmpty()){
+            regname.setError("Field cannot be empty")
+            return false;
+        }
+        else {
+            regname.setError(null)
+            return true;
+        }
+    }
+
+    private fun validateUser(): Boolean {
+        val val_user = reguser.text.toString()
+        val noWhiteSpace = ".*\\S+.*"
+
+        if (val_user.isEmpty()){
+            reguser.setError("Field cannot be empty")
+            return false;
+        }else if (val_user.count()>=15 ){
+            reguser.setError("Username too long")
+            return false
+        }else if (val_user.contains(" ")) {
+            reguser.setError("Spaces are not allowed")
+            return false
+        } else {
+            reguser.setError(null)
+            return true;
+        }
+    }
+
+    private fun validatePhone(): Boolean {
+        val val_phone = regphone.text.toString()
+
+        if (val_phone.isEmpty()) {
+            regphone.setError(null)
+            return true
+        } else if (val_phone.count()>9 || val_phone.count()<9) {
+            regphone.setError("Phone Number must be 9 numbers")
+            return false
+        } else {
+            regphone.setError(null)
+            return true
+        }
+    }
+
+    private fun validateNif(): Boolean {
+        val val_nif = regnif.text.toString()
+
+        if (val_nif.isEmpty()) {
+            regnif.setError(null)
+            return true;
+        }else if (val_nif.count()>9 || val_nif.count()<9) {
+            regnif.setError("Nif must be 9 numbers")
+            return false
+        }else {
+            regnif.setError(null)
+            return true;
+        }
+    }
+
+
+    private fun openLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    data class UserHelper(val userid: String? = null, val fullname: String? = null, val nif: String? = null, val phoneno: String? = null, val balance: Int? = null, val image: Int? = null) {
+        // Null default values create a no-argument default constructor, which is needed
+        // for deserialization from a DataSnapshot.
+    }
+}
--- glup/app/src/main/java/com/example/glup/RegistrationActivity.kt
+++ glup/app/src/main/java/com/example/glup/RegistrationActivity.kt
@@ -5,34 +5,36 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseAuth
 
 
 class RegistrationActivity : AppCompatActivity() {
 
     // initial variables
-    private lateinit var regname: EditText
-    private lateinit var reguser: EditText
+
     private lateinit var regemail:EditText
     private lateinit var regpass: EditText
-    private lateinit var regnif: EditText
-    private lateinit var regphone:EditText
+    private lateinit var reg_pass: EditText
+
+    private lateinit var myAuth: FirebaseAuth
 
 //------------------------------------------------------------------------------------------------------
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
+
+        myAuth= FirebaseAuth.getInstance()
         // get what was written
-        regname  = findViewById<EditText>(R.id.registration_full_name)
-        reguser  = findViewById<EditText>(R.id.registration_username)
+
         regemail = findViewById<EditText>(R.id.registration_email)
         regpass   = findViewById<EditText>(R.id.registration_password)
-        regnif   = findViewById<EditText>(R.id.registration_nif)
-        regphone = findViewById<EditText>(R.id.registration_phone)
+        reg_pass   = findViewById<EditText>(R.id.registration_password2)
+
+
 
         // When sign up button is pressed
-        findViewById<Button>(R.id.button_sign_up).setOnClickListener {
+        findViewById<Button>(R.id.button_next).setOnClickListener {
             RegisterUserActivity()
 
         }
@@ -44,72 +46,38 @@ class RegistrationActivity : AppCompatActivity() {
     }
 
 
+
     private fun RegisterUserActivity() {
 
-        if(!validateName() or !validateUser() or !validatePass() or !validateEmail() or !validatePhone() or !validateNif()) {
+        if(!validatePass() or !validateEmail()) {
             return
         }
 
-        // Prepare the database
-        val database = FirebaseDatabase.getInstance()
-        val newuser = database.getReference("users")
 
         // more variables and put the text in String format
-        val name = regname.text.toString()
-        val userid = reguser.text.toString()
+
         val email = regemail.text.toString()
         val pass = regpass.text.toString()
-        val nif = regnif.text.toString()
-        val phone = regphone.text.toString()
-        val balance = 0
-        val image = R.drawable.user_icon_app
 
-        // sending the info to data class
-        val new_user = UserHelper(userid, name, email, pass, nif, phone, balance, image)
-
-        // write in the database
-        newuser.child(userid).setValue(new_user)
+        createNewUser(email, pass)
 
         // going to next activity
-        val intent = Intent(this, MainActivity::class.java)
+        val intent = Intent(this, Registration2Activity::class.java)
         startActivity(intent)
 
     }
 
-    private fun validateName(): Boolean {
-        val val_name = regname.text.toString()
 
-        if (val_name.isEmpty()){
-            regname.setError("Field cannot be empty")
-            return false;
-        }
-        else {
-            regname.setError(null)
-            return true;
-        }
-    }
 
-    private fun validateUser(): Boolean {
-        val val_user = reguser.text.toString()
-        val noWhiteSpace = ".*\\S+.*"
+    private fun createNewUser(email: String?, password: String?) {
 
-        if (val_user.isEmpty()){
-            reguser.setError("Field cannot be empty")
-            return false;
-        }else if (val_user.count()>=15 ){
-            reguser.setError("Username too long")
-            return false
-        }else if (val_user.contains(" ")) {
-            reguser.setError("Spaces are not allowed")
-            return false
-        } else {
-            reguser.setError(null)
-            return true;
-        }
+        myAuth.createUserWithEmailAndPassword(email, password)
+
     }
 
     private fun validatePass(): Boolean {
         val val_pass = regpass.text.toString()
+        val val_pass2 = reg_pass.text.toString()
         val PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$"
 
         if (val_pass.isEmpty()){
@@ -120,10 +88,16 @@ class RegistrationActivity : AppCompatActivity() {
             regpass.setError("Password must be bigger than 7")
             return false
         }
-        else {
+        else if (val_pass!=val_pass2){
+            reg_pass.setError("Passwords must match")
+            return false;
+        }else {
+            reg_pass.setError(null)
             regpass.setError(null)
-            return true;
+            return true
         }
+
+
     }
 
     private fun validateEmail(): Boolean {
@@ -142,45 +116,11 @@ class RegistrationActivity : AppCompatActivity() {
         }
     }
 
-    private fun validatePhone(): Boolean {
-        val val_phone = regphone.text.toString()
-
-        if (val_phone.isEmpty()) {
-            regphone.setError(null)
-            return true
-        } else if (val_phone.count()>9 || val_phone.count()<9) {
-            regphone.setError("Phone Number must be 9 numbers")
-            return false
-        } else {
-            regphone.setError(null)
-            return true
-        }
-    }
-
-    private fun validateNif(): Boolean {
-        val val_nif = regnif.text.toString()
-
-        if (val_nif.isEmpty()) {
-            regnif.setError(null)
-            return true;
-        }else if (val_nif.count()>9 || val_nif.count()<9) {
-            regnif.setError("Nif must be 9 numbers")
-            return false
-        }else {
-            regnif.setError(null)
-            return true;
-        }
-    }
-
 
     private fun openLoginActivity() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
     }
 
-    data class UserHelper(val userid: String? = null, val fullname: String? = null, val email: String? = null, val password: String? = null, val nif: String? = null, val phoneno: String? = null,  val balance: Int? = null, val image: Int? = null ) {
-        // Null default values create a no-argument default constructor, which is needed
-        // for deserialization from a DataSnapshot.
-    }
-
  }
+
