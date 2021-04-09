83ea7f3ba7367290e91c88c359bbae6de3631e46
RRocha21 <renatorocha21@ua.pt>
2021-04-09 15:04:20 +0100

    Renato Rocha 1.1.24

--- glup/app/src/main/java/com/example/glup/MainActivity.kt
+++ glup/app/src/main/java/com/example/glup/MainActivity.kt
@@ -3,15 +3,30 @@ package com.example.glup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mfname: TextView
+    private lateinit var mbalance: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val ss:String = intent.getStringExtra("name").toString()
+        mfname = findViewById<TextView>(R.id.main_full_name)
+        mbalance = findViewById<TextView>(R.id.main_payment_label)
 
+        showdetails()
 
         findViewById<Button>(R.id.button_gps).setOnClickListener {
             openMapActivity()
@@ -28,6 +43,33 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button_settings).setOnClickListener {
             openSettingsActivity()
         }
+    }
+
+    private fun showdetails() {
+        val user= FirebaseAuth.getInstance().currentUser
+
+        val myRef= FirebaseDatabase.getInstance().getReference("users")
+
+
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                if (dataSnapshot.exists()) {
+
+                    val balancefromDb = dataSnapshot.child(user.uid).child("balance").getValue<Int>()
+                    val fullnamefromDb = dataSnapshot.child(user.uid).child("fullname").getValue<String>()
+
+                    mbalance.text= Editable.Factory.getInstance().newEditable(balancefromDb.toString().plus("$"))
+                    mfname.text = Editable.Factory.getInstance().newEditable(fullnamefromDb)
+
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+            }
+        }
+        myRef.addValueEventListener(postListener)
     }
 
     private fun openMapActivity() {
